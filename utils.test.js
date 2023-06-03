0cad115480e67898430fe420b5cbf6c115aab916
Mixed Nuts <jonfonts@gmail.com>
2023-06-03 06:20:57 -0500

    Added IsItemClassExcluded tests

--- utils.test.js
+++ utils.test.js
@@ -1,6 +1,6 @@
 //@ts-check
 
-import { IsItemClassIncluded, ItemWeight, LevelScaling, StatScaling, WeaponBaseDamage } from "./utils"
+import { IsItemClassExcluded, IsItemClassIncluded, ItemWeight, LevelScaling, StatScaling, WeaponBaseDamage } from "./utils"
 import { describe, expect, it } from "vitest"
 import { armorDef, attributeDefinitions, fireGem_t2, masterItem_fireStaff, masterItem_heavyFeet, masterItem_sword, wepDef } from "./variables"
 
@@ -39,4 +39,12 @@ describe("#IsItemClassIncluded", () => {
     it("MasterItem OMEGA_FireStaff ItemClass, returns true", () => expect(IsItemClassIncluded(masterItem_fireStaff, fireGem_t2)).toBe(true))
 
     it("MasterItem OMEGA_Sword ItemClass, returns true", () => expect(IsItemClassIncluded(masterItem_sword, fireGem_t2)).toBe(true))
+})
+
+describe("#IsItemClassExcluded", () => {
+    it("MasterItem OMEGA_HeavyFeet ItemClass, returns false", () => expect(IsItemClassExcluded(masterItem_heavyFeet, fireGem_t2)).toBe(false))
+
+    it("MasterItem OMEGA_FireStaff ItemClass, returns true", () => expect(IsItemClassExcluded(masterItem_fireStaff, fireGem_t2)).toBe(true))
+
+    it("MasterItem OMEGA_Sword ItemClass, returns false", () => expect(IsItemClassExcluded(masterItem_sword, fireGem_t2)).toBe(false))
 })
